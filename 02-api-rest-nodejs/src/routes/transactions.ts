ec402873dc440d1c3e776e328ef82a0258791be7
Henrry-Maximo <henrrylimadasilva@gmail.com>
2024-05-07 21:46:38 -0300

    feat: use plugins functionality of 'fastify'

---
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -0,0 +1,70 @@
+import fastify, { FastifyInstance } from "fastify";
+import { knex } from "../database";
+import { randomUUID } from "crypto";
+import { z } from "zod";
+
+// todo plugin do fastify, obrigatoriamente, precisa ser uma função assíncrona (await)
+// funcionalidade de plugins
+
+// marcação em app: específicar o formato
+export async function transactionsRoutes(app: FastifyInstance) {
+  // app.get("/hello", async () => {
+  //   const transactions = await knex("transactions")
+  //     .where("amount", 500)
+  //     .select("*");
+
+  //   return transactions;
+  // });
+
+  // listar todas as transactions:
+  // app.get('/transact')
+
+  // listar uma única transaction
+  // app.get('/transact/:id')
+
+  app.get("/", async (req, reply) => {
+    const transactions = await knex('transactions').select('*')
+
+    return transactions;
+  })
+
+  // rota para criação de uma nova transaction
+  app.post("/", async (req, reply) => {
+    // request.body: de onde o valor vem
+    // req. => armazena dezenas de informações
+
+    // obs.: req.body = objeto
+    const createTransacationBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    }); 
+
+    const { title, amount, type } = createTransacationBodySchema.parse(req.body);
+    // obs.: possível desestruturação
+
+    // alternativa => importar somente o módulo:
+    // import { randomUUID } from  "crypto"
+    // ou => import crypto from "crypto" = id: crypto.randomUUID()
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      // amount => debito = valor negativo / facilitar a soma (overview)
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+    // returning => geralmenta não utilizado em rotas de criação
+
+    // HTTP Codes () => Simbolizam o tipo de retorno que estou tendo da api
+    // 201 - Recurso Criado com Sucesso
+    // reply || response
+
+    // title, amount, type: credit ou debit
+    // aqui entra o pula do gato, utilizando o zod
+    // zod => validar o body && estabelecer formato
+    // const {} = req.body;
+
+    // express => response
+    // resposta vazia
+    return reply.status(201).send();
+  });
+}
--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -5,14 +5,22 @@ import crypto from 'node:crypto';
 
 import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 // base of application
 const app = fastify();
 
+// obs.: ordem que o fastify vai executar / ordem correta
+// segundo parâmetro: configuração
+// prefix => todas as rotas que comerarem com 'transactions' vai cair no plugin
+app.register(transactionsRoutes, {
+  prefix: 'transactions'
+});
+
 // GET, POST, PUT, PATCH, DELETE
 // http://localhost:333/hello
 
-app.get("/hello", async () => {
+// app.get("/hello", async () => {
   // const tables = await knex("sqlite_schema").select("*");
   // return tables;
 
@@ -29,10 +37,10 @@ app.get("/hello", async () => {
   // const transactions = await knex('transactions').select('*');
 
   // realizar uma busca de transações com where (onde?):
-  const transactions = await knex('transactions').where('amount', 500).select('*');
+  // const transactions = await knex('transactions').where('amount', 500).select('*');
 
-  return transactions;
-});
+  // return transactions;
+// });
 
 app
   .listen({
