c40380d20e85224f4debecd16542bf78d19df7b2
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-06-04 14:55:58 -0300

    feat: route of login

--- 02-desafio/src/routes/user.ts
+++ 02-desafio/src/routes/user.ts
@@ -1,17 +1,57 @@
 import { FastifyInstance } from "fastify";
 
+interface userType {
+  user: string;
+  password: string;
+}
+
+interface userArrayType {
+  userInDatabase: string;
+  passwordInDatabase: string;
+}
+
+const data:userArrayType[] = [
+  {
+    userInDatabase: "Henrique",
+    passwordInDatabase: "@123",
+  },
+];
+
 export async function userRoutes(app: FastifyInstance) {
+  app.post("/", (req, reply) => {
+    try {
+      const { user, password } = req.body as userType;
 
-  app.get("/", (req, reply) => {
-    const {user, password} = req.body;
+    if (!user || !password) {
+      return reply
+        .status(401)
+        .send({ message: "usuário ou senha são requeridos." });
+    }
 
-    // console.log("Login feito com sucesso!");
-    return reply.status(200).send({ message: "teste"});
+    const { userInDatabase, passwordInDatabase } = data[0];
+    console.log(data[0])
+
+    if (user == userInDatabase && password == passwordInDatabase) {
+      return reply
+        .status(200)
+        .send({
+          dados: `usuário: ${userInDatabase}, senha: ${passwordInDatabase}`,
+          message: "Login feito com sucesso!",
+        });
+    } else {
+      return reply
+        .status(401)
+        .send({
+          message: "Falha no login!",
+        });
+    }
+    
+    } catch(err) {
+      console.error(err);
+    }
   });
 
-  app.post("/registration", (req, reply) => {
-    const {user, password} = req.body;
-
-    
-  })
+  // app.post("/registration", (req, reply) => {
+  //   const { user, password } = req.body;
+  // });
 }
