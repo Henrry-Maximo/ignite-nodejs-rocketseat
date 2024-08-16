1b36b8fc77a21e5fad84e381fe42258d54b5be80
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-16 11:08:00 -0300

    ✨ feat: add ap/users-register route

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { prisma } from "./lib";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-prisma.user.create({
-  data: {
-    name: "Henrique",
-    email: "henrrylimadasilva@gmail.com",
-  },
+app.post("/users-register", async function (req, reply) {
+  const formatRegisterBody = z.object({
+    name: z.string(),
+    password: z.string(),
+    email: z.coerce.string().email().min(5),
+  });
+
+  const { name, password, email } = formatRegisterBody.parse(req.body);
+
+  await prisma.user.create({ data: { name, password_hash: password, email } });
+
+  return reply.status(201).send();
 });
--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -7,5 +7,5 @@ app
     port: env.PORT,
   })
   .then(() => {
-    console.log("🚀 HTTP Server Running!");
+    console.log(`🚀 HTTP Server Running! (${env.PORT})`);
   });
