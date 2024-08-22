47a3f792684b3dd79c2319b95dd09b92c201f344
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-22 10:52:27 -0300

    ✨feat: add vitest run coverage at app

--- 03-api-solid/src/use-cases/register.spec.ts
+++ 03-api-solid/src/use-cases/register.spec.ts
@@ -5,6 +5,19 @@ import { InMemoryUsersRespository } from "@/repositories/in-memory/in-memory-use
 import { UserAlreadyExistsError } from "./errors/user-already-exists";
 
 describe("Register Use Case", () => {
+  it("should be able to register", async () => {
+    const usersRepository = new InMemoryUsersRespository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
   it("should has user password up on registration", async () => {
     const usersRepository = new InMemoryUsersRespository();
     const registerUseCase = new RegisterUseCase(usersRepository);
@@ -35,9 +48,8 @@ describe("Register Use Case", () => {
       password: "123456",
     });
 
-    // Resolve / Reject
-
-    expect(() =>
+    // Resolve / Reject => Await Promise
+    await expect(() =>
       registerUseCase.execute({
         name: "John Doe",
         email,
