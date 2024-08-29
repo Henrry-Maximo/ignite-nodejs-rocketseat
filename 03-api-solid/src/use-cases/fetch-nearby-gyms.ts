5be1a6fc648d09db4d975edca91fec0e5c5e126b
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-29 11:00:28 -0300

    ✨feat: add use-cas of nearby gyms"

---
+++ 03-api-solid/src/use-cases/fetch-nearby-gyms.ts
@@ -0,0 +1,27 @@
+import { Gym } from "@prisma/client";
+import { GymsRepository } from "@/repositories/gyms-repository";
+
+interface FetchNearbyGymsUseCaseRequest {
+  userLatitude: number;
+  userLongitude: number;
+}
+
+interface FetchNearbyGymsUseCaseResponse {
+  gyms: Gym[];
+}
+
+export class FetchNearbyGymsUseCase {
+  constructor(private gymRepository: GymsRepository) {}
+
+  async execute({
+    userLatitude,
+    userLongitude,
+  }: FetchNearbyGymsUseCaseRequest): Promise<FetchNearbyGymsUseCaseResponse> {
+    const gyms = await this.gymRepository.findManyNearby({
+      latitude: userLatitude,
+      longitude: userLongitude,
+    });
+
+    return { gyms };
+  }
+}
