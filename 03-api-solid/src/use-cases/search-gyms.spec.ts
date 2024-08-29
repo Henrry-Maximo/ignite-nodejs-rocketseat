2768fdebaf88f174a98f806739badffb0f50c2a9
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-29 09:27:53 -0300

    ✨feat: add use-case search by gyms

---
+++ 03-api-solid/src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,66 @@
+import { InMemoryGymsRespository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { SearchGymsUseCase } from "./search-gyms";
+
+let gymsRepository: InMemoryGymsRespository;
+let sut: SearchGymsUseCase;
+
+describe("Search Gyms Use Case", () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRespository();
+    sut = new SearchGymsUseCase(gymsRepository);
+  });
+
+  it("should be able to search for gyms", async () => {
+    await gymsRepository.create({
+      title: "JavaScript",
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    });
+
+    await gymsRepository.create({
+      title: "TypeScript",
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    });
+
+    const { gyms } = await sut.execute({
+      query: "JavaScript",
+      page: 1,
+    });
+
+    console.log(`search: ${gyms}`);
+
+    expect(gyms).toHaveLength(1);
+    expect(gyms).toEqual([expect.objectContaining({ title: "JavaScript" })]);
+  });
+
+  it("should be able to fetch paginated gyms search", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `JavaScript Gym ${i}`,
+        description: null,
+        phone: null,
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      });
+    }
+
+    const { gyms } = await sut.execute({
+      query: "JavaScript",
+      page: 2,
+    });
+
+    console.log(`search with paginated: ${JSON.stringify(gyms, null, 2)}`);
+
+    expect(gyms).toHaveLength(2);
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: "JavaScript Gym 21" }),
+      expect.objectContaining({ title: "JavaScript Gym 22" }),
+    ]);
+  });
+});
---
+++ 03-api-solid/src/use-cases/search-gyms.ts
@@ -0,0 +1,24 @@
+import { Gym } from "@prisma/client";
+import { GymsRepository } from "@/repositories/gyms-repository";
+
+interface SearchGymsUseCaseRequest {
+  query: string;
+  page: number;
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[];
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymRepository.searchMany(query, page);
+
+    return { gyms };
+  }
+}
