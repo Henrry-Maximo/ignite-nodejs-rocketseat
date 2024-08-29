588e988e869fb6d2c584dab44fb0e600c6a2cd58
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-29 12:05:43 -0300

    ✨feat: add use-case-validation-inexistent-check-in-id"

--- 03-api-solid/src/use-cases/validate-check-in.spec.ts
+++ 03-api-solid/src/use-cases/validate-check-in.spec.ts
@@ -1,6 +1,7 @@
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { InMemoryCheckInsRespository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { ValidateCheckInUseCase } from "./validate-check-in";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 let checkInsRepository: InMemoryCheckInsRespository;
 let sut: ValidateCheckInUseCase;
@@ -29,6 +30,14 @@ describe("Validate Check-in Use Case", () => {
 
     expect(checkIn.validated_at).toEqual(expect.any(Date));
     expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date));
+  });
+
+  it("should not be able to validate an inexistent check-in", async () => {
+    await expect(() =>
+      sut.execute({
+        checkInId: "inexistent-check-in-id",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 });
 
