764744c9f12a212bd70574f4fc21281753705a44
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-04 16:20:13 -0300

    ✨feat: add@types/fastify-jwt.d.ts for definition of types

--- 03-api-solid/src/http/controllers/profile.ts
+++ 03-api-solid/src/http/controllers/profile.ts
@@ -1,1 +1,11 @@
-export async function profile() {}
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  // console.log(request.headers);
+  await request.jwtVerify(); // garantir autenticidade
+
+  console.log(request.user.sub);
+  console.log(request.user.iat);
+
+  return reply.status(200).send();
+}
