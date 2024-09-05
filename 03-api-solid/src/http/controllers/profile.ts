e61e37e256a35d33fbbf8856994156a9ca1f62c5
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-05 10:24:04 -0300

    ✨fea: add middlewares/verify-jwt.ts for working with informations of TokenJWT

--- 03-api-solid/src/http/controllers/profile.ts
+++ 03-api-solid/src/http/controllers/profile.ts
@@ -1,11 +1,20 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
-export async function profile(request: FastifyRequest, reply: FastifyReply) {
+export async function profile(req: FastifyRequest, reply: FastifyReply) {
   // console.log(request.headers);
-  await request.jwtVerify(); // garantir autenticidade
+  // await req.j wtVerify(); garantir autenticidade
 
-  console.log(request.user.sub);
-  console.log(request.user.iat);
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  return reply.status(200).send();
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  });
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
