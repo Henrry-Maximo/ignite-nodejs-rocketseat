22b8f05ad01c8d419ad3ca985a979a11840f5e96
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-06 17:02:30 -0300

    ✨feat: add environment test prisma config

---
+++ 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,14 @@
+import type { Environment } from "vitest";
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "ssr",
+  async setup() {
+    console.log("Setup!");
+    return {
+      teardown() {
+        console.log("Teardown!");
+      },
+    };
+  },
+};
