1a5f8070d3e14cc7114084695bc61bca257d05ea
Henrry-Maximo <henrrylimadasilva@gmail.com>
2024-09-09 20:06:58 -0300

    ✨ feat: add prisma isolated context for test e2e

--- 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,48 @@
+import "dotenv/config";
+
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 import type { Environment } from "vitest";
+import { PrismaClient } from "@prisma/client";
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+  return url.toString();
+}
+
+// connection database
+const prisma = new PrismaClient();
 
 export default <Environment>{
   name: "prisma",
   transformMode: "ssr",
   async setup() {
-    console.log("Setup!");
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    // deploy => jump verify
+    execSync("npx prisma migrate deploy");
+
+    // console.log("Setup!");
     return {
       async teardown() {
-        console.log("Teardown!");
+        // console.log("Teardown!");
+
+        // operation delete schema at cascade
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        );
+
+        // closed connection database
+        await prisma.$disconnect();
       },
     };
   },
