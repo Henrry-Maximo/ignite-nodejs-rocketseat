488313cb1a23c4b146d741cda3406a8a471e5711
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-10 10:18:55 -0300

    ✨feat: add Supertest for test of register client at users route

--- 03-api-solid/src/http/controllers/register.spec.ts
+++ 03-api-solid/src/http/controllers/register.spec.ts
@@ -1,3 +1,31 @@
-import { test } from "vitest";
+import { app } from "@/app";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {});
+interface PropsRegister {
+  name: string;
+  email: string;
+  password: string;
+}
+
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server)
+      .post("/users")
+      .send(<PropsRegister>{
+        name: "Johh Doe",
+        email: "johndoe@example.com",
+        password: "@123456",
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
