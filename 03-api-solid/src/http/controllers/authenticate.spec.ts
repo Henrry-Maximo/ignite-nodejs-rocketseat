ca903f36aace47a35a6246bd8f7a00d8484b7116
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-11 10:04:50 -0300

    ✨feat: add controllers/authenticate.spec.ts test e2e at app

---
+++ 03-api-solid/src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,39 @@
+import { app } from "@/app";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+interface PropsRegister {
+  name: string;
+  email: string;
+  password: string;
+}
+
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await request(app.server)
+      .post("/users")
+      .send(<PropsRegister>{
+        name: "Johh Doe",
+        email: "johndoe@example.com",
+        password: "@123456",
+      });
+
+    const response = await request(app.server)
+      .post("/sessions")
+      .send(<PropsRegister>{
+        email: "johndoe@example.com",
+        password: "@123456",
+      });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ token: expect.any(String) });
+  });
+});
