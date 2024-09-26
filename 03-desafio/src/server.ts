6440c4165e89ddbe69a0ce31b0021c940e0fe44b
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-09-26 12:23:30 -0300

    🎉 First Commit Challenge

---
+++ 03-desafio/src/server.ts
@@ -0,0 +1,10 @@
+import { app } from "./app";
+import { env } from "./env";
+
+app
+	.listen({
+		port: env.SERVER_PORT,
+	})
+	.then(() => {
+		console.log(`🚀 HTTP Server Running, port: ${env.SERVER_PORT}`);
+	});
