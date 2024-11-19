cf8edff167e4464f6ae1f7cc6177e0a12a7dfd0d
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-11-19 16:22:07 -0300

    ✨Feat: Add register pets http

--- 03-desafio/src/http/controllers/pets/register.ts
+++ 03-desafio/src/http/controllers/pets/register.ts
@@ -1,4 +1,39 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
 
-export async function register(req, reply) {
-  
+export async function register(req: FastifyRequest, reply: FastifyReply) {
+	const typingRegisterBody = z.object({
+		name: z.string(),
+		description: z.string().max(300),
+		age: z.enum([""]),
+		size: z.enum([""]),
+		power: z.enum([""]),
+		independence: z.enum([""]),
+		ambience: z.enum([""]),
+		requisite: z.string(),
+	});
+
+	const {
+		name,
+		description,
+		age,
+		size,
+		power,
+		independence,
+		ambience,
+		requisite
+	} = typingRegisterBody.parse(req.body);
+
+  try {
+    const registerUseCase = makeRegisterUseCase();
+
+    await registerUseCase.execute({
+      name, description, age, size, power, independence, ambience, requisite
+    })
+
+  } catch (error) {
+    return reply.status(500).send({
+      error
+    })
+  }
 }
