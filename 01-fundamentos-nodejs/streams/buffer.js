eb9b4b9b444272cee728a473f2c836139d61b044
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-05-06 19:31:09 -0300

    📚  Docs: Add comments at codebase

--- 01-fundamentos-nodejs/streams/buffer.js
+++ 01-fundamentos-nodejs/streams/buffer.js
@@ -6,3 +6,22 @@ const buf = Buffer.from(
 // Binário && Hexadecimal
 console.log(buf.toJSON("utf8"));
 // The Buffers are populated with strings
+
+// JS have a problem of not be performative with data binary (hexadecimal)
+// TypedArray  -> Module native for working with read and write in memory of the browser
+// write and read in memory, method simple using comunication at format binary
+
+const nameFormattingInBuffer = Buffer.from("Henrique");
+console.log(nameFormattingInBuffer); // <Buffer 48 65 6e 72 69 71 75 65>
+
+console.log(buf.toJSON());  // Append format decimal
+// { 
+//    type: 'Buffer', 
+//    data: [
+//            82, 111,  99, 107, 101, 116,  83, 
+//            101,  97, 116,  32,  73, 115,  32, 
+//            86, 101, 114, 121,  32,  71, 111, 
+//            111, 100,  33
+//    ]
+// }
+// Memory computer -> work only with values binary
