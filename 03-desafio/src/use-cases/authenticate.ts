5551919a149c20f560fc35ff750902e150c87a7b
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-07-11 20:08:41 -0300

    ✨ Feat: Add authenticate org with verify data

---
+++ 03-desafio/src/use-cases/authenticate.ts
@@ -0,0 +1,41 @@
+import { OrgsRepository } from "@/repositories/orgs-repository";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { compare } from "bcryptjs";
+import { Org } from "@prisma/client";
+
+interface AuthenticateUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+// return void
+// type AuthenticateUseCaseResponse = void;
+interface AuthenticateUseCaseResponse {
+  org: Org;
+}
+
+export class AuthenticateUseCase {
+  constructor(private orgsRepository: OrgsRepository) {
+
+  }
+
+  async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    // auth
+    const org = await this.orgsRepository.findByEmail(email);
+
+    if (!org) {
+      throw new InvalidCredentialsError();
+    }
+
+    // Boolean => "is", "has", "does"
+    const doesPassowordMatches = await compare(password, org.password_hash);
+
+    if (!doesPassowordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      org,
+    };
+  }
+}
