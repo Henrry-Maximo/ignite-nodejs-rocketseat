ac2c2a897f725a10b02f49f48796bf6bdfef62e9
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-07-14 19:46:22 -0300

    ✨ Feat: Add authenticate controller and route

---
+++ 03-desafio/src/http/controllers/authenticate.ts
@@ -0,0 +1,37 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+
+import { PrismaOrgsRepository } from "@/repositories/prisma/prisma-orgs-repository";
+import { AuthenticateUseCase } from "@/use-cases/authenticate";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+
+export const authenticate = async (req: FastifyRequest, reply: FastifyReply) => {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(32),
+  });
+
+  const { email, password} =
+    authenticateBodySchema.parse(req.body);
+
+  try {
+    const prismaOrgsRepository = new PrismaOrgsRepository();
+    const authenticateUseCase = new AuthenticateUseCase(prismaOrgsRepository);
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(409).send({ message: err.message });
+    }
+
+    throw err;
+  }
+
+  // Operações de criação / atualização / remoção : não há necessidade de retorno
+  return reply
+    .status(200)
+    .send();
+};
--- 03-desafio/src/http/routes.ts
+++ 03-desafio/src/http/routes.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
+import { authenticate } from "./controllers/authenticate";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/orgs", register);
+
+  /*
+    * não utilizar verbo, mas sim, semântico.
+    * authenticate -> sessions.
+  */
+  app.post("/sessions", authenticate);
 }
