330b7b418cd44d5282a63333e40b5f8562cc7816
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-08-17 23:00:27 -0300

    ✨ Feat: Add mapping relationship

--- 04-clean-ddd/src/domain/entities/answer.ts
+++ 04-clean-ddd/src/domain/entities/answer.ts
@@ -1,16 +1,26 @@
 import { randomUUID } from "node:crypto";
 
+interface AnswerProps {
+  content: string;
+  authorId: string;
+  questionId: string;
+}
+
 export class Answer {
   /**
-   * @param id valor único (opcional, será gerado automaticamente se não 
+   * @param id valor único (opcional, será gerado automaticamente se não
    * fornecido - situações em que precisa atualizar um dado já existente)
   */
- 
+
   public id: string;
   public content: string;
+  public authorId: string;
+  public questionId: string;
 
-  constructor(content: string, id?: string) {
+  constructor(props: AnswerProps, id?: string) {
     this.id = id ?? randomUUID();
-    this.content = content;
+    this.content = props.content;
+    this.questionId = props.questionId;
+    this.authorId = props.authorId;
   }
 }
--- 04-clean-ddd/src/domain/entities/question.ts
+++ 04-clean-ddd/src/domain/entities/question.ts
@@ -1,20 +1,27 @@
 import { randomUUID } from "node:crypto";
 
+interface QuestionProps {
+  title: string;
+  content: string;
+  authorId: string;
+}
+
 export class Question {
   /**
-   * @param id valor único (opcional, será gerado automaticamente se não 
+   * @param id valor único (opcional, será gerado automaticamente se não
    * fornecido - situações em que precisa atualizar um dado já existente)
-  */
- 
+   */
+
   public id: string;
   public title: string;
   public content: string;
   public authorId: string;
 
-  constructor(title: string, content: string, authorId: string, id?: string) {
+  constructor(props: QuestionProps, id?: string) {
+    // Object.assign(this, props)
     this.id = id ?? randomUUID();
-    this.title = title;
-    this.authorId = authorId;
-    this.content = content;
+    this.title = props.title;
+    this.authorId = props.authorId;
+    this.content = props.content;
   }
 }
