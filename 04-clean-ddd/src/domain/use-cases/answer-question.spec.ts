3eaa9f144e19ca44c8ce2fcfd58fe5b4f5682c2d
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-09-08 09:34:31 -0300

    ✨ Feat: Add config for using imports of vitest without perform the import

--- 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
+++ 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question.js";
 import { AnswersRepository } from "../repositories/answers-repository.js";
 import { Answer } from "../entities/answer.js";
