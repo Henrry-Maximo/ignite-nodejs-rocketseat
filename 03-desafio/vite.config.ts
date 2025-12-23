a50f36346f970de32077d454a5c9362846c01ef0
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-12-23 16:03:02 -0300

    ✨ Feat: Add config for tests unit and e2e, using databases different

--- 03-desafio/vite.config.ts
+++ 03-desafio/vite.config.ts
@@ -6,4 +6,25 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
   ],
+  test: {
+    dir: "src",
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases'
+        }
+      },
+
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment: './prisma/vitest-environment-prisma/prisma-test-environment.ts'
+        }
+      }
+    ]
+  }
 });
