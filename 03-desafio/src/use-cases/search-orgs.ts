5f269076560f80e9560038dc404ba72e5f1aefee
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-12-27 13:59:42 -0300

    ✨ Feat: Remove password_hash organizations of return  in search

--- 03-desafio/src/use-cases/search-orgs.ts
+++ 03-desafio/src/use-cases/search-orgs.ts
@@ -2,18 +2,26 @@ import { OrgsRepository } from "@/repositories/orgs-repository";
 import { Org } from "@prisma/client";
 
 interface SearchOrgsUseCaseRequest {
-  name?: string
+  name?: string;
 }
 
 interface SearchOrgsUseCaseResponse {
-  orgs: Org[]
+  orgs: Omit<Org, "password_hash">[];
 }
 
 export class SearchOrgsUseCase {
   constructor(private orgRepository: OrgsRepository) {}
 
-  async execute({ name }: SearchOrgsUseCaseRequest): Promise<SearchOrgsUseCaseResponse> {
-    const orgs = await this.orgRepository.searchMany(name);
+  async execute({
+    name,
+  }: SearchOrgsUseCaseRequest): Promise<SearchOrgsUseCaseResponse> {
+    const responseOrgs = await this.orgRepository.searchMany(name);
+
+    const orgs = responseOrgs.map((org) => {
+      const { password_hash, ...orgWithoutPassword } = org;
+      
+      return orgWithoutPassword;
+    });
 
     return { orgs };
   }
