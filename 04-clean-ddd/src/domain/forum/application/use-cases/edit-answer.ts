d0739ea570c7f3875c6f9cfcdacc6bae34af80e9
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-06 18:32:56 -0300

    ✨ Feat: Add return of edit question/answer

--- 04-clean-ddd/src/domain/forum/application/use-cases/edit-answer.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/edit-answer.ts
@@ -1,3 +1,4 @@
+import { Answer } from '../../enterprise/entities/answer';
 import { AnswersRepository } from '../repositories/answers-repository'
 
 interface EditAnswerUseCaseRequest {
@@ -6,7 +7,9 @@ interface EditAnswerUseCaseRequest {
   content: string;
 }
 
-interface EditAnswerUseCaseResponse {}
+interface EditAnswerUseCaseResponse {
+  answer: Answer;
+}
 
 export class EditAnswerUseCase {
   constructor(private answersRepository: AnswersRepository) {}
@@ -30,6 +33,8 @@ export class EditAnswerUseCase {
 
     await this.answersRepository.save(answer)
 
-    return {}
+    return {
+      answer
+    }
   }
 }
