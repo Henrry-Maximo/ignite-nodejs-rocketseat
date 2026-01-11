c53d7427506adb96b8a8159725451ab4aca83ba8
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-11 16:48:15 -0300

    ✨ Feat: Add use case fetch answer comments and spec test

--- 04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
 
@@ -17,6 +18,12 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
     
     return answerComment
   };
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items.filter((item) => item.answerId.toString() === answerId).slice((page - 1) * 20, page * 20);
+
+    return answerComments;
+  }
 
   async delete(answerComment: AnswerComment) {
     const itemIndex = this.items.findIndex((item) => item.id === answerComment.id);
