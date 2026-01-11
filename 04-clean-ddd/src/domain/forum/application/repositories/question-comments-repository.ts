270efd5a8a712deada365d72cc85918942a5f20e
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-11 16:40:17 -0300

    ✨ Feat: Add fetch question comments and spec test

--- 04-clean-ddd/src/domain/forum/application/repositories/question-comments-repository.ts
+++ 04-clean-ddd/src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
 import { QuestionComment } from '../../enterprise/entities/question-comment';
 
 export interface QuestionCommentsRepository {
   create(questionComment: QuestionComment): Promise<void>;
+  findManyByQuestionId(questionId: string, params: PaginationParams): Promise<QuestionComment[]>;
   findById(id: string): Promise<QuestionComment | null>;
   delete(questionComment: QuestionComment): Promise<void>
 }
--- 04-clean-ddd/test/repositories/in-memory-question-comments-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
@@ -22,5 +23,11 @@ export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepos
     const itemIndex = this.items.findIndex((item) => item.id === questionComment.id);
 
     this.items.splice(itemIndex, 1);
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComments = this.items.filter((item) => item.questionId.toString() === questionId).slice((page - 1) * 20, page * 20);
+
+    return questionComments;
   }
 }
