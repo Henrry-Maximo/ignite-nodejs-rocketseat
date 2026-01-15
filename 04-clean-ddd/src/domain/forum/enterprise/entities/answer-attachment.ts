0cb0a0bc4ff6480d1d8c20ea3559f692be90eb87
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-15 17:16:09 -0300

    ✨ Feat: Add entities answer/question attachments

---
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -0,0 +1,29 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+/*
+  * Contexto: como se fosse uma tabela bivô no banco de dados, quando temos o relacionamento
+  * de N:N. Mas não necessariamente precisar virar uma tabela no banco de dados.
+  * Motivo: na camada de domínio, serve como um caminho intermediário que possibilita realizar a conexão entre duas entidades.
+*/
+
+interface AnswerAttachmentProps {
+  answerId: UniqueEntityID;
+  attachmentId: UniqueEntityID;
+}
+
+export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
+  get answerId() {
+    return this.props.answerId;
+  }
+
+  get attachmentId() {
+    return this.props.attachmentId;
+  }
+
+  static create(props: AnswerAttachmentProps, id?: UniqueEntityID) {
+    const attachment = new AnswerAttachment(props, id);
+
+    return attachment;
+  }
+}
---
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/question-attachment.ts
@@ -0,0 +1,29 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+/*
+  * Contexto: como se fosse uma tabela bivô no banco de dados, quando temos o relacionamento
+  * de N:N. Mas não necessariamente precisar virar uma tabela no banco de dados.
+  * Motivo: na camada de domínio, serve como um caminho intermediário que possibilita realizar a conexão entre duas entidades.
+*/
+
+interface QuestionAttachmentProps {
+  questionId: UniqueEntityID;
+  attachmentId: UniqueEntityID;
+}
+
+export class QuestionAttachment extends Entity<QuestionAttachmentProps> {
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get attachmentId() {
+    return this.props.attachmentId;
+  }
+
+  static create(props: QuestionAttachmentProps, id?: UniqueEntityID) {
+    const attachment = new QuestionAttachment(props, id);
+
+    return attachment;
+  }
+}
