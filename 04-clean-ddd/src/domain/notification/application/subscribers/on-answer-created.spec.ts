01dfed6d001cb749349c2aa7e819b34ae850c930
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-01-30 19:34:33 -0300

    ✨ Feat: Add send a notification on answer created spec

--- 04-clean-ddd/src/domain/notification/application/subscribers/on-answer-created.spec.ts
+++ 04-clean-ddd/src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -2,15 +2,31 @@ import { makeAnswer } from "test/factories/make-answer";
 import { OnAnswerCreated } from "./on-answer-created";
 import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
 import { InMemoryAnswerAttachmentsRepository } from "test/repositories/in-memory-answer-attachments-repository";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { SendNotificationUseCase } from "../use-cases/send-notification";
+import { InMemoryNotificationsRepository } from "test/repositories/in-memory-notifications-repository";
 
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository;
+let sendNotificationUseCase: SendNotificationUseCase;
 
 describe('On Answer Created', () => {
   beforeEach(() => {
-    inMemoryAnswerAttachmentsRepository = new InMemoryAnswerAttachmentsRepository;
+    inMemoryQuestionAttachmentsRepository = new InMemoryQuestionAttachmentsRepository();
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository
+    );
+    inMemoryAnswerAttachmentsRepository = new InMemoryAnswerAttachmentsRepository();
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
       inMemoryAnswerAttachmentsRepository
+    );
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository();
+    sendNotificationUseCase = new SendNotificationUseCase(
+      inMemoryNotificationsRepository
     );
   });
 
