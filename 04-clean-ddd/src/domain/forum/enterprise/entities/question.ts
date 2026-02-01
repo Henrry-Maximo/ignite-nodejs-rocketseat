09d85ada157ffd7427b836382cebcf84b7718bd9
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-01 20:20:14 -0300

    🐛  Fix: Validation best answer id for create domain event

--- 04-clean-ddd/src/domain/forum/enterprise/entities/question.ts
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/question.ts
@@ -86,13 +86,17 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-    if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
+    if (bestAnswerId === undefined) {
+      return;
+    }
+
+    if (this.props.bestAnswerId === undefined || !this.props.bestAnswerId.equals(bestAnswerId)) {
       this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
     }
 
+    // if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {}
+
     this.props.bestAnswerId = bestAnswerId;
-
-
 
     this.touch();
   }
