de997d930b299064240ad26c22dce97046a1c4e3
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-04 20:03:45 -0300

    ✨ Feat: Add create question controller

--- 05-nest-clean/src/controllers/create-question.controller.ts
+++ 05-nest-clean/src/controllers/create-question.controller.ts
@@ -1,24 +1,66 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, UsePipes } from "@nestjs/common";
 import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { type UserPayload } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import z from "zod";
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {};
+  constructor(
+    private prisma: PrismaService
+  ) {};
   
-  // @Post()
-  // async handle(@Req() request: Request) {
-  //   console.log(request.user);
-    
-  //   return 'ok';
-  // }
-
+  
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub);
+  // @UsePipes(new ZodValidationPipe(createQuestionBodySchema)) // not more necessary use 
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload
+  ) {
+    const { title, content } = body;
+    const { sub: userId } = user;
+
+    const slug = this.convertToSlug(title);
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug
+      }
+    });
     
-    return 'ok';
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .normalize('NFKD')
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, '-') // Substitui espaços por hífen
+      .replace(/[^\w-]+/g, '') // Remove tudo que não é palavra ou hífen
+      .replace(/_/g, '-') // Substitui sublinhado por hífen
+      .replace(/--+/g, '-') // Substitui múltiplos hífen por um
+      .replace(/^-|-$/g, '') // Remove hífen do início ou fim
   }
 }
+
+// @Post()
+// async handle(@Req() request: Request) {
+//   console.log(request.user);
+  
+//   return 'ok';
+// }
