b9bf3a21d1c66d154b377f97304fb0aa5bf3105c
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-07 18:49:38 -0300

    ✨ Feat: Add fetch recent questions test spec

--- 05-nest-clean/src/controllers/fetch-recent-questions.controller.ts
+++ 05-nest-clean/src/controllers/fetch-recent-questions.controller.ts
@@ -27,7 +27,7 @@ export class FetchRecentQuestionsController {
   async handle(
     @Query('page', queryValidationPipe) page: PageQueryParamSchema
   ) {
-    const perPage = 1;
+    const perPage = 20;
 
     const questions = await this.prisma.question.findMany({
       take: perPage,
