f2643d77596029ecb661b5db07bb418907a2b65b
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-02-07 18:41:15 -0300

    ✨ Feat: Add create question test spec

---
+++ 05-nest-clean/src/controllers/create-question.controller.e2e-spec.ts
@@ -0,0 +1,59 @@
+// test("1 plus 1", () => {
+//   expect(1 + 1).toBe(2);
+// })
+
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+
+describe('Create question (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    // subir a aplicação de forma programática
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+
+  test('[POST] /questions', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }
+    });
+
+    const accessToken = jwt.sign({ sub: user.id });
+
+    const response = await request(app.getHttpServer()).post('/questions').set('Authorization', `Bearer ${accessToken}`).send({
+      title: 'New Question',
+      content: "Question content"
+    });
+
+    expect(response.statusCode).toBe(201);
+
+    const userOnDatabase = await prisma.question.findFirst({
+      where: {
+        title: 'New Question',
+      }
+    });
+
+    expect(userOnDatabase).toBeTruthy();
+  });
+
+})
