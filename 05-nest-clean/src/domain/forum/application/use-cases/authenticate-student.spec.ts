4f253dd92e0f102eb295577f554fee2c06004ae1
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-08-22 22:06:47 -0300

    ✨ feat: add authenticate student spec

---
+++ 05-nest-clean/src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -0,0 +1,45 @@
+import { RegisterStudentUseCase } from "./register-student.js";
+import { InMemoryStudentsRepository } from "test/repositories/in-memory-students-repository.js";
+import { FakeHasher } from "test/cryptography/fake-hasher.js";
+import { FakeEncrypter } from "test/cryptography/fake-encrypter.js";
+import { AuthenticateStudentUseCase } from "./authenticate-student.js";
+import { makeStudent } from "test/factories/make-student.js";
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
+let fakeHasher: FakeHasher;
+let fakeEncrypter: FakeEncrypter;
+
+let sut: AuthenticateStudentUseCase;
+
+describe("Register Student", () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
+    fakeHasher = new FakeHasher();
+    fakeEncrypter = new FakeEncrypter();
+
+    sut = new AuthenticateStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    );
+  });
+
+  it("should be able to authenticate a student", async () => {
+    const student = makeStudent({
+      email: "johndoe@example.com",
+      password: await fakeHasher.hash('123456'), 
+    });
+
+    inMemoryStudentsRepository.items.push(student);
+
+    const result = await sut.execute({
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      student: expect.any(String),
+    });
+  });
+});
