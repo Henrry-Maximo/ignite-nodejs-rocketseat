b69f2a0b61b846475a4e8fc1d89aba4ea1aebf50
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-08-27 23:43:31 -0300

    ✨ feat: add class factory with method make prisma student for e2e tests

--- 05-nest-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ 05-nest-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,46 +1,57 @@
 import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
 import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
 import request from "supertest";
+import { StudentFactory } from "test/factories/make-student";
 
 describe("Get question by slug (E2E)", () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let studentFactory: StudentFactory
   let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [StudentFactory, DatabaseModule]
     }).compile();
 
     app = moduleRef.createNestApplication();
 
+    // injeção da dependência
     prisma = moduleRef.get(PrismaService);
+    studentFactory = moduleRef.get(StudentFactory)
     jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
 
   test("[GET] /questions/:slug", async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: "John Doe",
-        email: "johndoe@example.com",
-        password: "123456",
-      },
-    });
+    // const user = await prisma.user.create({
+    //   data: {
+    //     name: "John Doe",
+    //     email: "johndoe@example.com",
+    //     password: "123456",
+    //   },
+    // });
 
-    const accessToken = jwt.sign({ sub: user.id });
+    const user = await studentFactory.makePrismaStudent();
 
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    // utilizando o mapper para converter os dados da camada de domínio para de persistência
+    // const question = makeQuestion();
     await prisma.question.create({
       data: {
         title: "Question 01",
         slug: "question-01",
         content: "Question content",
-        authorId: user.id,
+        authorId: user.id.toString(),
       },
+      // data: PrismaQuestionMapper.toPrisma(question)
     });
 
     const response = await request(app.getHttpServer())
--- 05-nest-clean/test/factories/make-student.ts
+++ 05-nest-clean/test/factories/make-student.ts
@@ -1,7 +1,10 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Student, StudentProps } from '@/domain/forum/enterprise/entities/student'
+import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeStudent(
   override: Partial<StudentProps> = {},
@@ -18,4 +21,22 @@ export function makeStudent(
   )
 
   return student
+}
+
+@Injectable()
+export class StudentFactory {
+  constructor(
+    private prisma: PrismaService
+  ) {}
+
+  async makePrismaStudent(data: Partial<StudentProps> = {}): Promise<Student> {
+    const student = makeStudent(data);
+
+    await this.prisma.user.create({
+      data: PrismaStudentMapper.toPrisma(student)
+    });
+
+    return student;
+  }
+
 }
