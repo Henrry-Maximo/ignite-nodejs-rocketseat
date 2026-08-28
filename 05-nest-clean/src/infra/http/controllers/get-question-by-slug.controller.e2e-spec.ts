e37992e02511080481286d7e0ac45ada1f17cbe1
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-08-28 00:06:36 -0300

    ✨ feat: add student and question factories in the fetch recent question controller e2e

--- 05-nest-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ 05-nest-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 import { AppModule } from "@/infra/app.module";
 import { DatabaseModule } from "@/infra/database/database.module";
-import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
@@ -11,7 +10,7 @@ import { StudentFactory } from "test/factories/make-student";
 
 describe("Get question by slug (E2E)", () => {
   let app: INestApplication;
-  let prisma: PrismaService;
+  // let prisma: PrismaService;
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
   let jwt: JwtService;
@@ -25,7 +24,7 @@ describe("Get question by slug (E2E)", () => {
     app = moduleRef.createNestApplication();
 
     // injeção da dependência
-    prisma = moduleRef.get(PrismaService);
+    // prisma = moduleRef.get(PrismaService);
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
     jwt = moduleRef.get(JwtService);
