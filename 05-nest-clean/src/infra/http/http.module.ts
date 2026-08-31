0572ad0fc83486c4522def8011a986c12309462d
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-08-31 02:25:32 -0300

    ✨ feat: add fetch answer comments controller and e2e spec

--- 05-nest-clean/src/infra/http/http.module.ts
+++ 05-nest-clean/src/infra/http/http.module.ts
@@ -33,6 +33,8 @@ import { DeleteAnswerCommentController } from "./controllers/delete-answer-comme
 import { DeleteAnswerCommentUseCase } from "@/domain/forum/application/use-cases/delete-answer-comment";
 import { FetchQuestionCommentsController } from "./controllers/fetch-question-comments.controller";
 import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-question-comments";
+import { FetchAnswerCommentsController } from "./controllers/fetch-answer-comments.controller";
+import { FetchAnswerCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-answer-comments";
 
 @Module({
   imports: [
@@ -56,7 +58,8 @@ import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cas
     DeleteQuestionCommentController,
     CommentOnAnswerController,
     DeleteAnswerCommentController,
-    FetchQuestionCommentsController
+    FetchQuestionCommentsController,
+    FetchAnswerCommentsController
   ],
   providers: [
     // {
@@ -80,7 +83,8 @@ import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cas
     DeleteQuestionCommentUseCase,
     CommentOnAnswerUseCase,
     DeleteAnswerCommentUseCase,
-    FetchQuestionCommentsUseCase
+    FetchQuestionCommentsUseCase,
+    FetchAnswerCommentsUseCase
   ],
   // providers: [
   //   PrismaService
